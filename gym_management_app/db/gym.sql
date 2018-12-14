8d1476dee28ab84fb46119d251835e52b380afea
Mohamed Eshaftri <eshaftri@gmail.com>
2018-12-14 11:34:56 +0000

    file layout

--- gym_management_app/db/gym.sql
+++ gym_management_app/db/gym.sql
@@ -1,11 +1,11 @@
-DROP TABLE attendauts;
+DROP TABLE attendants;
 DROP TABLE members;
 DROP TABLE events;
 
 
 CREATE TABLE members
 (
-  id SERIAL8 primay key,
+  id SERIAL8 primary key,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255),
   titel VARCHAR(255),
@@ -15,16 +15,16 @@ CREATE TABLE members
 
 CREATE TABLE events
 (
-  id SERIAL8 primay key,
+  id SERIAL8 primary key,
   event_type VARCHAR(255),
   event_date VARCHAR(255),
   event_time VARCHAR(255),
   event_size VARCHAR(255)
 );
 
-CREATE TABLE attendauts
+CREATE TABLE attendants
 (
-  id SERIAL8 primay key,
+  id SERIAL8 primary key,
   member_id INT8 references  members(id),
   event_id INT8 references events(id)
 );
